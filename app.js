2294157f5fa9154567f803ae5c1999c72dd46e8c
robinkatiyar <103071357+robinkatiyar@users.noreply.github.com>
2024-05-25 03:49:33 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,10 @@
+const student = {
+    name:"karan",
+    age:15,
+    class:9,
+    username:"karan@123",
+    password:"abcd",
+    location:"Mumbai"
+}
+let {username:user , password:pwd}=student;
+console.log(user,pwd)
